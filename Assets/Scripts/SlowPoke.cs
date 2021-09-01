2bd51c0e3bf25275574edd32771e67091e14f752
Sai <thegeekskm101@gmail.com>
2021-08-31 22:15:31 -0500

    Added the Slow Enemy
    called that sucker the slowpoke. I think I made it pretty well.

---
+++ Assets/Scripts/SlowPoke.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowPoke : Enemy
+{
+    [SerializeField] float _slowAmount = 0.1f;
+
+    protected override void PlayerImpact(Player player)
+    {
+        //base.PlayerImpact(player);
+        if (player != null)
+        {
+            TankController tankController = player.GetComponent<TankController>();
+            if (tankController != null)
+            {
+                tankController.MaxSpeed -= _slowAmount;
+            }
+        }
+    }
+}
