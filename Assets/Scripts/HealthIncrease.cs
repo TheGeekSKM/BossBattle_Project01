bee736400708d87540a5838d95ae8b334b2ff3f1
Sai <thegeekskm101@gmail.com>
2021-09-19 22:58:48 -0500

    Added Coins Back
    just added some stuff back

---
+++ Assets/Scripts/HealthIncrease.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthIncrease : CollectibleBase
+{
+    protected override void Collect(Player player)
+    {
+        //throw new System.NotImplementedException();
+        player.PlayerHealth.CurrentHealth++;
+    }
+
+    protected override void Movement(Rigidbody rb)
+    {
+        Quaternion turnOffset = Quaternion.Euler(2, MovementSpeed, 2);
+        rb.MoveRotation(GetComponent<Rigidbody>().rotation * turnOffset);
+
+        Vector3 moveOffset = new Vector3(0, 0, (-1 * MoveSpeed));
+        rb.MovePosition(GetComponent<Rigidbody>().position + moveOffset);
+    }
+}
