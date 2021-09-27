546270f2708831f8f8f6920c3518b4c955716ea8
Sai <thegeekskm101@gmail.com>
2021-09-27 11:35:10 -0500

    Menu Finished
    I finished the Menu. I plan on adding a How to Play Menu, but for now, this works.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] GameObject _menuScreen;
+    [SerializeField] GameObject _creditsScreen;
+    public void OpenMenu()
+    {
+        _menuScreen.SetActive(true);
+        _creditsScreen.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        _menuScreen.SetActive(false);
+        _creditsScreen.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
